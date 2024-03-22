1e82054a69c2e78b2bf2e9ce7953e5e357823309
Stéphany Lahass Silva <20201TIIMI0292@cefetes.br>
2024-03-22 08:45:34 -0300

    Initial commit

---
+++ app/src/main/java/silva/lahass/primeiroapp/NextActivity.java
@@ -0,0 +1,20 @@
+package silva.lahass.primeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        Intent i = getIntent();
+        String texto = i.getStringExtra("texto");
+        TextView tvTexto = findViewById(R.id.tvTexto);
+        tvTexto.setText(texto);
+    }
+}
